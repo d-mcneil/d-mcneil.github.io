dfdfde9cd2e06e1dc7a4f1d142b7a47982535b26
Dean McNeil <deanolmcneil@gmail.com>
2023-03-23 16:02:17 -0400

    Update site to match resume

---
+++ index.e8da0e2f.js
@@ -0,0 +1,7 @@
+const handleCopyEmail = ()=>{
+    navigator.clipboard.writeText("dean.mcneil.developer@gmail.com");
+};
+const copyEmailButton = document.getElementById("copy-email-button");
+copyEmailButton.addEventListener("click", handleCopyEmail);
+
+//# sourceMappingURL=index.e8da0e2f.js.map
